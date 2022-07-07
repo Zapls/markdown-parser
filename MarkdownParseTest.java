3ea80ce3715afaf811995460314e5cb947128e7f
Zapls <91322645+Zapls@users.noreply.github.com>
2022-07-07 09:14:18 -0700

    created new JUnit test for getLinks method

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,27 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
 import org.junit.*;
 public class MarkdownParseTest {
 
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void TestGetLinks() throws IOException {
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        ArrayList<String> actualL = new ArrayList<>(Arrays.asList(new String[]{"https://something.com", "some-thing.html"}));
+        assertEquals(links, actualL);
     }
 
 }
--- test-file2.md
+++ test-file2.md
@@ -2,5 +2,4 @@
 
 [a link!](https://google.com)
 [another link!](some-thing.html)
-
 some paragraph text after the links
